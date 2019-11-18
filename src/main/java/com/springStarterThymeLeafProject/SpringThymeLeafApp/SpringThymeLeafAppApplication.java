5c19bc153b70524c5c4777268be200136e9f060d
zan <clement.mortas@le-campus-numerique.fr>
2019-11-18 14:49:00 +0100

    first commit

---
+++ src/main/java/com/springStarterThymeLeafProject/SpringThymeLeafApp/SpringThymeLeafAppApplication.java
@@ -0,0 +1,13 @@
+package com.springStarterThymeLeafProject.SpringThymeLeafApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeLeafAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeLeafAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springStarterThymeLeafProject/SpringThymeLeafApp/SpringThymeLeafAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springStarterThymeLeafProject.SpringThymeLeafApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeLeafAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
